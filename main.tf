67d0e597f01c80edacaf4a3ad4852250f4597c9c
omor13 <fnsfbk@gmail.com>
2024-02-22 12:27:38 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  cloud {
+    organization = "Bynet1"
+
+    workspaces {
+      name = "WorkCLI"
+    }
+  }
+   required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+provider "aws" {
+    region = "eu-central-1"
+}
+
+# New testing VPC
+resource "aws_vpc" "test_vpc" {
+    cidr_block = "10.0.0.0/24"
+}
